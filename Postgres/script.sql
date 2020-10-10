2accf4b3c9e532ff9726e2febf1a3b01624602e2
Cuong <bdcuong91@gmail.com>
2020-10-10 14:49:10 +0200

    add schema db

--- Postgres/script.sql
+++ Postgres/script.sql
@@ -1,10 +1,11 @@
-create table contacts (
+create schema db;
+create table db.contacts (
     uid serial,
     email VARCHAR(80) not null,
     first_name VARCHAR(80) NOT NULL,
     last_name VARCHAR(80) NOT NULL
 );
-INSERT INTO contacts(email, first_name, last_name) VALUES('jim@example.com', 'Jim', 'Smith');
-INSERT INTO contacts(email, first_name, last_name) VALUES(null, 'John', 'Smith');
-INSERT INTO contacts(email, first_name, last_name) VALUES('carol@example.com', 'Carol', 'Smith');
-INSERT INTO contacts(email, first_name, last_name) VALUES('sam@example.com', 'Sam', null);
+INSERT INTO db.contacts(email, first_name, last_name) VALUES('jim@example.com', 'Jim', 'Smith');
+INSERT INTO db.contacts(email, first_name, last_name) VALUES(null, 'John', 'Smith');
+INSERT INTO db.contacts(email, first_name, last_name) VALUES('carol@example.com', 'Carol', 'Smith');
+INSERT INTO db.contacts(email, first_name, last_name) VALUES('sam@example.com', 'Sam', null);
